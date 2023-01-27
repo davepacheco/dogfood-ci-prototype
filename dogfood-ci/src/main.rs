ed25f51b4640119db8450f71b869f5e72eab6ce5
David Pacheco <dap@oxidecomputer.com>
2023-01-27 09:33:01 -0800

    initial skeleton

---
+++ dogfood-ci/src/main.rs
@@ -0,0 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Prototype program that uses the Oxide API for a CI system
+//! The goal of this program is to derisk the plan for using an Oxide system for
+//! buildomat-based CI.
